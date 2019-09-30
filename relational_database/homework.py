37cd5eac1806863b77df2ad3e3823212dace3a76
arturUkr <arturukrnem@gmail.com>
2019-09-30 21:58:01 +0300

    add placeholder

--- relational_database/homework.py
+++ relational_database/homework.py
@@ -21,7 +21,8 @@ def task_1_add_new_record_to_db(con) -> None:
     """
     with con.cursor() as cursor:
         cursor.execute(
-            "INSERT INTO Customers VALUES(92, 'Thomas', 'David', 'Some Address', 'London', '774', 'Singapore');"
+            "INSERT INTO Customers VALUES(%s, %s, %s, %s, %s, %s, %s);",
+            (92, 'Thomas', 'David', 'Some Address', 'London', '774', 'Singapore')
         )
 
 
